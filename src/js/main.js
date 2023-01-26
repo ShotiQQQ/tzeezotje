82ea007b283bed32264544580602d4ebe51bac89
Denis <shotiqqq@gmail.com>
2023-01-26 12:51:25 +0700

    add section contacts and add yandex map

--- src/js/main.js
+++ src/js/main.js
@@ -12,4 +12,25 @@ const swiperPhotos = new Swiper('.swiper', {
   },
   slidesPerView: 'auto',
   spaceBetween: 30,
+  centeredSlides: true,
+  initialSlide: 1,
 });
+
+ymaps.ready(init);
+function init(){
+  var myMap = new ymaps.Map("map", {
+    center: [51.050461, 3.743162],
+    zoom: 10
+  });
+
+  var myGeoObject = new ymaps.GeoObject({
+    geometry: {
+      type: "Point",
+      coordinates: [51.008176, 3.628080],
+    }
+  });
+
+  myMap.behaviors.disable('scrollZoom');
+  myMap.geoObjects.add(myGeoObject);
+}
+
